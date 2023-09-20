168d076f0bd4a8735d18484ff05e85b94608ecd9
Aggie-254 <agnesmwangangi98@gmail.com>
2023-09-20 15:33:59 -0700

    little change

--- read_stream.c
+++ read_stream.c
@@ -1,49 +1,49 @@
 #include"shell.h"
 
 /**
- * read_line - read a line from the stream
+ * read_stream - read a line from the stream
  *
  * Return: pointer that points the the read line
  */
 char *read_stream(void)
 {
-    int bufsize = 1024;
-    int i = 0;
-    char *line = malloc(sizeof(char) * bufsize);
-    int character;
+int bufsize = 1024;
+int i = 0;
+char *line = malloc(sizeof(char) * bufsize);
+int character;
 
-    if (line == NULL)
-    {
-        fprintf(stderr, "allocation error in read_stream");
-        exit(EXIT_FAILURE);
-    }
-    while (1)
-    {
-        character = getchar();
-        if (character == EOF)
-        {
-            free(line);
-            exit(EXIT_SUCCESS);
-        }
-        else if (character == '\n')
-        {
-            line[i] = '\0';
-            return (line);
-        }
-        else
-        {
-            line[i] = character;
-        }
-        i++;
-        if (i >= bufsize)
-        {
-            bufsize += bufsize;
-            line = realloc(line, bufsize);
-            if (line == NULL)
-            {
-                fprintf(stderr, "reallocation error in read_stream");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }
+if (line == NULL)
+{
+fprintf(stderr, "allocation error in read_stream");
+exit(EXIT_FAILURE);
+}
+while (1)
+{
+character = getchar();
+if (character == EOF)
+{
+free(line);
+exit(EXIT_SUCCESS);
+}
+else if (character == '\n')
+{
+line[i] = '\0';
+return (line);
+}
+else
+{
+line[i] = character;
+}
+i++;
+if (i >= bufsize)
+{
+bufsize += bufsize;
+line = realloc(line, bufsize);
+if (line == NULL)
+{
+fprintf(stderr, "reallocation error in read_stream");
+exit(EXIT_FAILURE);
+}
+}
+}
 }
